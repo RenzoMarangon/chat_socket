431c66499661f2c7ab61d88da7ff5dd2f18d707a
Renzo Marangon <renzomarangon@gmail.com>
2023-07-25 20:05:48 -0300

    v1.0 | Server completo con creacion y autenticacion de usuario a traves de JWT

---
+++ controllers/auth.js
@@ -0,0 +1,64 @@
+const { response } = require("express");
+const bcryptjs = require('bcryptjs');
+
+const User = require('../models/user');
+const { generateJWT } = require("../database/generate-jwt");
+
+
+const login = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        //Verificar email
+        const user = await User.findOne({ email })
+
+        if(!user)
+        {
+            return res.status(400).json({
+                msg:'User or password incorrect'
+            })
+        }
+
+        //Verificar user active
+        if(!user.state)
+        {
+            return res.status(400).json({
+                msg:"User doesn't exist"
+            })
+        }
+        
+        //Verificar password
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if(!validPassword)
+        {
+            return res.status(400).json({
+                msg:'User or password incorrect'
+            })
+        }
+
+        //Generar JWT
+        const token = await generateJWT( user.id );        
+
+        res.status(500).json({
+            user,
+            token
+        })
+ 
+    } catch (error) {
+        console.log(err);
+        res.status(500).json({
+            msg:'Something went wrong'
+        })
+    }
+
+}
+
+
+
+
+module.exports = {
+    login,
+    
+}
---
+++ models/server.js
@@ -0,0 +1,52 @@
+const cors = require('cors')
+const express = require('express');
+const { dbConnection } = require('../database/config');
+
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;     
+
+        //MONGOOSE
+        this.connectDB()
+
+        //MIDDLEWARES
+        this.middlewares();
+
+        //ROUTES
+        this.routes();
+    }
+
+
+    async connectDB(){
+        await dbConnection();
+    }
+
+    middlewares() {
+        //CORS
+          this.app.use(cors({
+            origin: '*'
+          }));            
+
+        //Lectura y <parseo del body
+        this.app.use( express.json()  );
+
+        //Pagina publica
+        this.app.use( express.static('public') );
+    }
+
+    routes(){
+        this.app.use( '/api/auth', require('../routes/auth') );
+        this.app.use('/api/users',require('../routes/users'));
+
+    }
+
+    listen(){
+        this.app.listen( this.port, ()=>{
+            console.log(`Server listening port ${ this.port }`)
+        })
+    }
+}
+
+
+module.exports = Server;
